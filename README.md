c38912f74142920c22fbff2a23ad28af43635200
aarekr <aare212@hotmail.com>
2025-04-13 18:23:16 +0300

    Navigation to SummaryScreen, bottomNavigationBar

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# personalfinancetracker
+# Personal Finance Tracker
+
+* Application for tracking daily expenses
+* Written in Flutter (Dart)
--- app/lib/main.dart
+++ app/lib/main.dart
@@ -6,6 +6,7 @@ void main() {
     initialRoute: "/",
     getPages: [
       GetPage(name: "/", page: () => HomeScreen()),
+      GetPage(name: "/summary", page: () => SummaryScreen()),
     ]
   ));
 }
@@ -13,10 +14,39 @@ void main() {
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Center(child: Text("Personal Finance Tracker")),
+        ),
+        body: Center(
+          child: Text('Personal Finance Tracker'),
+        ),
+        bottomNavigationBar: Container(
+          height: 50,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              OutlinedButton(
+                child: Text("Summary"),
+                onPressed: ()  => Get.to(() => SummaryScreen()),
+              ),
+            ]
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SummaryScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Personal Finance Tracker'),
+          child: Text('Summary'),
         ),
       ),
     );
