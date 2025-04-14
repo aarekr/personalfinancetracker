5acb3eca19b2d8c660a3600bfedbfe0b99e94bb3
aarekr <aare212@hotmail.com>
2025-04-14 14:38:39 +0300

    SummaryScreen navigation, appbar, bottomnavbar

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
 
 void main() {
   runApp(GetMaterialApp(
@@ -43,10 +45,26 @@ class HomeScreen extends StatelessWidget {
 class SummaryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Center(child: Text("Personal Finance Tracker")),
+        ),
         body: Center(
           child: Text('Summary'),
+        ),
+        bottomNavigationBar: Container(
+          height: 50,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              OutlinedButton(
+                child: Text("Home"),
+                onPressed: ()  => Get.to(() => HomeScreen()),
+              ),
+            ]
+          ),
         ),
       ),
     );
